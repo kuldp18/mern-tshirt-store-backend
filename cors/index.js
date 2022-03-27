468f75bc559ef811b7165cece9ab2b944addf6a1
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-03-27 08:30:00 +0530

    added local origin

--- cors/index.js
+++ cors/index.js
@@ -1,6 +1,6 @@
 const cors = require('cors');
 const corsOptions = {
-  origin: 'https://mern-tshirt-store.netlify.app',
+  origin: ['https://mern-tshirt-store.netlify.app', 'http://localhost:3000'],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
